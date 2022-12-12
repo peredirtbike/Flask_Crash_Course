652dca3e2defa2b0e4075a9b03dff6b768a2ec73
Pere Garcia <peredirtbike@gmail.com>
2022-12-12 11:59:27 +0100

    Adding employees finished

--- Flask_CrudApp/app.py
+++ Flask_CrudApp/app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost:3306/flask_crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def Index():
-    return render_template("index.html")
+    all_data = Data.query.all()
+
+    return render_template("index.html", employees = all_data)
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+    
+        my_data = Data(name,email,phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Employee Inserted Successfully")
+        
+        return redirect(url_for('Index'))
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
